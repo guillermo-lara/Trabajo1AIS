4e0f7f46440eba57426c341eaa1fc9e6c80fec5a
guillermo-lara <g.lara.2022@alumnos.urjc.es>
2025-03-11 10:22:27 +0100

    INTEGRACIÓN HECHO

--- Nitflex/src/test/java/es/codeurjc/web/nitflex/integration/FilmTestIntegrationsUtils.java
+++ Nitflex/src/test/java/es/codeurjc/web/nitflex/integration/FilmTestIntegrationsUtils.java
@@ -1,22 +1,47 @@
 package es.codeurjc.web.nitflex.integration;
 
-import static org.junit.jupiter.api.Assertions.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.File;
-import java.io.FileInputStream;
-import java.io.IOException;
-import java.io.InputStream;
-import java.sql.Blob;
-import java.sql.SQLException;
+import java.util.Optional;
 
 import org.junit.jupiter.api.Test;
-import org.springframework.mock.web.MockMultipartFile;
-import org.springframework.web.multipart.MultipartFile;
+import org.mapstruct.factory.Mappers;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
 
+import es.codeurjc.web.nitflex.dto.film.CreateFilmRequest;
+import es.codeurjc.web.nitflex.dto.film.FilmDTO;
+import es.codeurjc.web.nitflex.dto.film.FilmMapper;
+import es.codeurjc.web.nitflex.model.Film;
+import es.codeurjc.web.nitflex.repository.FilmRepository;
+import es.codeurjc.web.nitflex.service.FilmService;
+import jakarta.transaction.Transactional;
+
+@SpringBootTest
+@Transactional
 public class FilmTestIntegrationsUtils {
-    //@Test
+    @Autowired
+    private FilmService filmService;
+    @Autowired
+    private FilmRepository filmRepository;
 
-    
+    @Test
+    public void testAFilm() {
+
+        FilmMapper filmMapper = Mappers.getMapper(FilmMapper.class);
+
+        CreateFilmRequest createfilmRequest = new CreateFilmRequest("Test Movie", "Test Synopsis", 2024, "PG-13");
+
+        FilmDTO filmDTO = filmService.save(createfilmRequest);
+        assertNotNull(filmDTO);
+
+        Film film = filmMapper.toDomain(filmDTO);
+
+        Optional<Film> filmFromRep = filmRepository.findById(film.getId());
+        assertTrue(filmFromRep.isPresent());
+        assertEquals("Test Movie", filmFromRep.get().getTitle());
+
+    }
 }
-
-
--- Nitflex/target/generated-sources/annotations/es/codeurjc/web/nitflex/dto/film/FilmMapperImpl.java
+++ Nitflex/target/generated-sources/annotations/es/codeurjc/web/nitflex/dto/film/FilmMapperImpl.java
@@ -14,7 +14,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-10T23:15:24+0100",
+    date = "2025-03-11T09:43:13+0100",
     comments = "version: 1.6.3, compiler: Eclipse JDT (IDE) 3.41.0.z20250213-2037, environment: Java 21.0.6 (Eclipse Adoptium)"
 )
 @Component
