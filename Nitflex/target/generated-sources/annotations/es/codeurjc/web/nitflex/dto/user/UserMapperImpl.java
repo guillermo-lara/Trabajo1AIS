e69a3185d4a16ba74e33104a57e0abe39d0d3ddd
guillermo-lara <g.lara.2022@alumnos.urjc.es>
2025-03-28 13:15:28 +0100

    UserService Update

--- Nitflex/target/generated-sources/annotations/es/codeurjc/web/nitflex/dto/user/UserMapperImpl.java
+++ Nitflex/target/generated-sources/annotations/es/codeurjc/web/nitflex/dto/user/UserMapperImpl.java
@@ -15,7 +15,7 @@ import org.springframework.stereotype.Component;
 
 @Generated(
     value = "org.mapstruct.ap.MappingProcessor",
-    date = "2025-03-28T13:09:06+0100",
+    date = "2025-03-28T13:15:15+0100",
     comments = "version: 1.6.3, compiler: javac, environment: Java 23.0.2 (Oracle Corporation)"
 )
 @Component
